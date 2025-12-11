69ba1de6f90c1ae73cb4dca0c08a227daae0d9b9
Diana <dianadorojuc3@gmail.com>
2025-12-11 18:17:08 +0700

    cancellati i commenti

--- src/api/authApi.ts
+++ src/api/authApi.ts
@@ -1,7 +1,6 @@
 import type { User } from "../types/auth";
 import emptyApi from "./emptyApi";
 import { authStarted, authSuccess, authFailed } from '../store/authSlice';
-//const API_BASE_URL = 'http://localhost:3001';  // backend Spring (URL base)
 
 // Risposta login (cosa ricevo dal backend quando faccio il login)
 interface LoginResponce {
@@ -26,35 +25,7 @@ interface LoginBody {
   password: string;
 }
 
-// SOLO le chiamate API: qui non modifico lo stato Redux (prende i dati dal server)
-// Login (POST /auth/login)
-/* export async function loginApi(email: string, password: string): Promise<LoginResponce> {
-    const res = await fetch(`${API_BASE_URL}/auth/login`, {
-        method: 'POST',
-        headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify({ email, password }),
-    });
-    if (!res.ok) {
-        const message = await res.text();
-        throw new Error(message || "Login failed");
-    }
-    return res.json();
-}
 
-// Register
-export async function registerApi(body: RegisterBody): Promise<User> {    // Restituirà una Promise che contiene un oggetto di tipo User
-    const res = await fetch(`${API_BASE_URL}/auth/register`, {
-        method: 'POST',
-        headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify(body),
-    });
-    if (!res.ok) {
-        const message = await res.text();
-        throw new Error(message || "Registration failed");
-    }
-    return res.json();
-}
- */
 export const authApi = emptyApi.injectEndpoints({
   endpoints: (build) => ({
     //Post /auth/login
